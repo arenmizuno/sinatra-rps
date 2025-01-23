e8b606ba389784b351ca30f3874043583d1f4b9e
Aren Mizuno <166631743+arenmizuno@users.noreply.github.com>
2025-01-23 18:01:03 +0000

    final

--- app.rb
+++ app.rb
@@ -2,8 +2,47 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+# Simulate rock
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+  erb(:rock)
+end
+
+# Simulate paper
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+  erb(:paper)
+end
+
+# Simulate scissors
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+  erb(:scissors)
 end
